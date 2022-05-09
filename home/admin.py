d9af018c52a2c36e76ca487281c655debeb59b3f
Noglo Cléa <62757034+cleason@users.noreply.github.com>
2022-05-09 12:19:51 +0200

    Commit

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import BlogModel
 
 # Register your models here.
+
+@admin.register(BlogModel)
+class AdminBlogModel(admin.ModelAdmin):
+    list_display = ('title', 'created_at', 'upload_to')
+    prepopulated_fields = {'slug': ('title',), }
--- home/models.py
+++ home/models.py
@@ -1,16 +1,18 @@
-from distutils.command.upload import upload
-from email.mime import image
-from turtle import title
 from django.db import models
 from django.contrib.auth.models import User
+from froala_editor.fields import FroalaField
 
 # Create your models here.
 
 class BlogModel(models.Model):
     title = models.CharField(max_length=1000)
-    content = models.TextField()
     slug = models.SlugField(max_length=1000)
+    content = FroalaField()
     image = models.ImageField(upload_to = 'blog')
     created_at = models.DateTimeField(auto_now_add=True)
     upload_to = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.title
+    
     
